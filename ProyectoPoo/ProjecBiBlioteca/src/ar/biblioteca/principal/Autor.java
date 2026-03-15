0cabed9b4e3fada13ae80e33c0d2341c99406add
Ríos Bruno <161901966+brunorios21@users.noreply.github.com>
2026-03-15 12:30:45 -0300

    Update Autor.java

--- ProyectoPoo/ProjecBiBlioteca/src/ar/biblioteca/principal/Autor.java
+++ ProyectoPoo/ProjecBiBlioteca/src/ar/biblioteca/principal/Autor.java
@@ -7,27 +7,44 @@ public class Autor {
     private String nombre;
     private String apellido;
     private int dni;
-    private List<Libro> libros; // Relación 1 a muchos
+    private List<Libro> libros;
 
     public Autor() {
         this.libros = new ArrayList<>();
     }
 
-    // Método especial para vincular
     public void agregarLibro(Libro nuevoLibro) {
         this.libros.add(nuevoLibro);
-        nuevoLibro.setTitular(this); // El libro ahora sabe quién es su autor
+        nuevoLibro.setTitular(this);
     }
 
-    // Getters y Setters
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
+    // Getters
+    public String getNombre() { 
+        return nombre; 
+    }
 
-    public String getApellido() { return apellido; }
-    public void setApellido(String apellido) { this.apellido = apellido; }
+    public String getApellido() { 
+        return apellido; 
+    }
 
-    public int getDni() { return dni; }
-    public void setDni(int dni) { this.dni = dni; }
+    public int getDni() { 
+        return dni; 
+    }
 
-    public List<Libro> getLibros() { return libros; }
+    public List<Libro> getLibros() { 
+        return libros; 
+    }
+
+    // Setters
+    public void setNombre(String nombre) { 
+        this.nombre = nombre; 
+    }
+
+    public void setApellido(String apellido) { 
+        this.apellido = apellido; 
+    }
+
+    public void setDni(int dni) { 
+        this.dni = dni; 
+    }
 }
