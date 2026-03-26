2fe3a070339d79bf984c56bcb41714f5b19c27f2
Ríos Bruno <161901966+brunorios21@users.noreply.github.com>
2026-03-26 09:48:26 -0300

    Update Contacto.java
    me gusta asi!

--- ProyectoPoo/src/ar/com/unpaz/principal/Contacto.java
+++ ProyectoPoo/src/ar/com/unpaz/principal/Contacto.java
@@ -9,18 +9,38 @@ public class Contacto {
     private String email;
 
     // Métodos de acceso (Getters y Setters)
-    public String getLocalidad() { return localidad; }
-    public void setLocalidad(String localidad) { this.localidad = localidad; }
+    public String getLocalidad() { 
+        return localidad; 
+    }
+    public void setLocalidad(String localidad) { 
+        this.localidad = localidad; 
+    }
 
-    public String getCalle() { return calle; }
-    public void setCalle(String calle) { this.calle = calle; }
+    public String getCalle() { 
+        return calle; 
+    }
+    public void setCalle(String calle) { 
+        this.calle = calle; 
+    }
 
-    public int getNumero() { return numero; }
-    public void setNumero(int numero) { this.numero = numero; }
+    public int getNumero() { 
+        return numero; 
+    }
+    public void setNumero(int numero) { 
+        this.numero = numero; 
+    }
 
-    public int getCodigoPostal() { return codigoPostal; }
-    public void setCodigoPostal(int codigoPostal) { this.codigoPostal = codigoPostal; }
+    public int getCodigoPostal() { 
+        return codigoPostal; 
+    }
+    public void setCodigoPostal(int codigoPostal) { 
+        this.codigoPostal = codigoPostal; 
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getEmail() { 
+        return email; 
+    }
+    public void setEmail(String email) { 
+        this.email = email; 
+    }
 }
